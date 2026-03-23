e0136b38709612b598e17e63a92a4effac31b94b
pfasantos <pedro.felp2203@gmail.com>
2026-03-23 09:11:58 -0300

    change buffer name + translate comments

--- main/i2s_std.c
+++ main/i2s_std.c
@@ -4,20 +4,20 @@ i2s_chan_handle_t rx_handle;
 
 void i2s_init()
 {
-    // configurações do canal
+    // i2s channel configuration 
     i2s_chan_config_t chan_cfg = {
         .id = I2S_NUM_AUTO,
         .role = I2S_ROLE_MASTER,
-        .dma_desc_num = DMA_BUF_NUM,   // quantidade de buffers do DMA
-        .dma_frame_num = DMA_BUF_SIZE, // tamanho dos buffers do DMA
+        .dma_desc_num = DMA_BUF_NUM,
+        .dma_frame_num = DMA_BUF_SIZE,
         .auto_clear = false,
     };
 
     i2s_new_channel(&chan_cfg, NULL, &rx_handle);
 
-    // configuracoes de clock slot e gpio
+    // i2s std mode configuration
     i2s_std_config_t std_cfg = {
-        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(8000), // inicia num clock menor
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(8000),    // start channel in slower clock
         .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(BIT_DEPTH, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
             .mclk = I2S_GPIO_UNUSED,
@@ -33,7 +33,6 @@ void i2s_init()
         },
     };
 
-    // inicializar o canal i2s no modo std
     i2s_channel_init_std_mode(rx_handle, &std_cfg);
     ESP_LOGI(I2S_TAG, "Canal I2S iniciado.");
 }
--- main/i2s_std.h
+++ main/i2s_std.h
@@ -8,12 +8,14 @@
 #include "driver/i2s_pdm.h"
 #include "driver/gpio.h"
 
+//tags
 #define I2S_TAG "i2s"
 
-#define BIT_DEPTH I2S_DATA_BIT_WIDTH_32BIT
-#define DMA_BUF_NUM 16                           // quantidade de buffers do DMA
-#define DMA_BUF_SIZE 511                         // tamanho em amostras dos buffers do DMA
-#define BUF_SIZE 2 * DMA_BUF_SIZE *BIT_DEPTH / 8 // tamanho em bytes dos buffers de transmissao
+//macros
+#define BIT_DEPTH I2S_DATA_BIT_WIDTH_32BIT                 // i2s bit depth
+#define DMA_BUF_NUM 16                                     // quantity of dma buffers 
+#define DMA_BUF_SIZE 511                                   // number of samples of dma buffer 
+#define I2S_BUF_SIZE_BYTES 2 * DMA_BUF_SIZE *BIT_DEPTH / 8 // size in bytes of i2s buffer 
 
 extern i2s_chan_handle_t rx_handle;
 
